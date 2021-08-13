a1be2560b9773ba2bb2ec8131715f7315da8daac
Joseph Biju Cadavil <joseph.biju.cadavil@sap.com>
2021-08-13 21:49:13 +0530

    Model Basics 101

--- src/Table/list-component.js
+++ src/Table/list-component.js
@@ -1,70 +1,71 @@
-import React from 'react';
-import 'react-smart-data-table/dist/react-smart-data-table.css';
-import SmartDataTable from 'react-smart-data-table';
-import {
-  BrowserRouter as Router,
-  Link,
-  useLocation
-} from "react-router-dom";
+import React from "react";
+import "react-smart-data-table/dist/react-smart-data-table.css";
+import SmartDataTable from "react-smart-data-table";
+import { Button, Header, Icon, Modal } from "semantic-ui-react";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Link, useLocation } from "react-router-dom";
 
 function ListComponent() {
   const emptyTableD = <div>There is no data available at the time.</div>;
-  const base_url = 'https://api.spacexdata.com/v3/launches';
+  const base_url = "https://api.spacexdata.com/v3/launches";
+
+  const [modelVisible, setModelVisible] = useState(false);
+  const [modeldata, setModelData] = useState();
   const headers = {
     columnKey: {
-      text: '-',
+      text: "-",
       invisible: true,
       sortable: false,
-      filterable: false
+      filterable: false,
     },
     _id: {
-      text: 'No',
+      text: "No",
       invisible: false,
       sortable: false,
-      filterable: true
+      filterable: true,
     },
     launch_date_utc: {
-      text: 'Launched (UTC)',
+      text: "Launched (UTC)",
       invisible: false,
       sortable: false,
       filterable: true,
       transform: (value, index, row) => {
         return funDate(value);
-      }
+      },
     },
     location: {
-      text: 'Location',
+      text: "Location",
       invisible: false,
       sortable: false,
-      filterable: false
+      filterable: false,
     },
     mission: {
-      text: 'Mission',
+      text: "Mission",
       invisible: false,
       sortable: false,
-      filterable: false
+      filterable: false,
     },
     orbit: {
-      text: 'Orbit',
+      text: "Orbit",
       invisible: false,
       sortable: false,
-      filterable: false
+      filterable: false,
     },
     launchState: {
-      text: 'Launched Status',
+      text: "Launched Status",
       invisible: false,
       sortable: false,
-      filterable: true
+      filterable: true,
     },
     rocket: {
-      text: 'Rocket',
+      text: "Rocket",
       invisible: false,
       sortable: false,
-      filterable: false
-    }
+      filterable: false,
+    },
   };
 
-  const dataResolver = response => {
+  const dataResolver = (response) => {
     let filteredResp = [];
     for (var i = 0; i < response.length; i++) {
       filteredResp.push({
@@ -74,11 +75,11 @@ function ListComponent() {
         mission: response[i].mission_name,
         orbit: response[i].rocket.second_stage.payloads[0].orbit, //TODO: Error Handle
         launchState: response[i].launch_success
-          ? 'SUCCESS'
+          ? "SUCCESS"
           : response[i].upcoming
-          ? 'UPCOMING'
-          : 'FAILED',
-        rocket: response[i].rocket.rocket_name
+          ? "UPCOMING"
+          : "FAILED",
+        rocket: response[i].rocket.rocket_name,
       });
     }
     //console.log(filteredResp);
@@ -88,29 +89,75 @@ function ListComponent() {
   const onRowClick = (event, { rowData, rowIndex, tableData }) => {
     // The following results should be identical
     console.log(rowData, tableData[rowIndex]);
+    setModelData(rowData);
+    setModelVisible(true);
   };
 
-  //TODO: Lazy loading 
+  const onModelClose = (event, data) => {
+    console.log(event);
+    setModelVisible(false);
+  };
+
+  //TODO: Lazy loading
   let filters = useQuery();
-  let statusFilter = filters.get("status")
-  
-  let api = base_url
+  let statusFilter = filters.get("status");
+
+  let api = base_url;
 
   return (
-    <SmartDataTable
-      data={api}
-      dataKey="_id"
-      dataKeyResolver={dataResolver}
-      name="space-x"
-      className="ui compact selectable table"
-      sortable
-      filterValue={statusFilter}
-      emptyTable={emptyTableD}
-      showOnRowClick={true}
-      perPage={20}
-      onRowClick={onRowClick}
-      headers={headers}
-    />
+    <div>
+      <SmartDataTable
+        data={api}
+        dataKey="_id"
+        dataKeyResolver={dataResolver}
+        name="space-x"
+        className="ui compact selectable table"
+        sortable
+        filterValue={statusFilter}
+        emptyTable={emptyTableD}
+        showOnRowClick={true}
+        perPage={20}
+        onRowClick={onRowClick}
+        headers={headers}
+      />
+      <Modal
+        open={modelVisible}
+        size="small"
+        closeIcon={true}
+        onClose={onModelClose}
+      >
+        <Header icon>
+          <Icon name="archive" />
+          Archive Old Messages
+        </Header>
+        <Modal.Content>
+          <table class="ui table">
+            <tbody>
+              <tr>
+                <td>John</td>
+                <td>{modeldata? modeldata._id: ""}</td>
+              </tr>
+              <tr>
+                <td>Jamie</td>
+                <td>Approved</td>
+              </tr>
+              <tr>
+                <td>Jill</td>
+                <td>Denied</td>
+              </tr>
+            </tbody>
+          </table>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button basic color="red" inverted>
+            <Icon name="remove" /> No
+          </Button>
+          <Button color="green" inverted>
+            <Icon name="checkmark" /> Yes
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    </div>
   );
 }
 
